05cf77e89a318528a5087a0c13672834381c052e
badrjj26 <badrjj26@narancoinformatica.local>
2025-09-25 17:43:50 +0200

    Modifico fichero Ejemplo2While.java

--- src/ejercicios/depuracion/Ejemplo2While.java
+++ src/ejercicios/depuracion/Ejemplo2While.java
@@ -5,6 +5,14 @@ package ejercicios.depuracion;
  */
 import java.util.*;
 public class Ejemplo2While {
+
+    public static int pedirNumero(){
+        Scanner sc = new Scanner(System.in)
+        System.out.print("Introduce un numero: ");
+        return sc.nextInt();
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n, contador = 0;
